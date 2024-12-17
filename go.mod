fc9aa2c4a472f61068917d8559acaf820bd704ad
vviveksharma <visharma@progress.com>
2024-12-17 16:50:55 +0530

    Minor update

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/GIT_USER_ID/GIT_REPO_ID
+module github.com/Password-Management/API-Client
 
 go 1.18
 
--- test/api_customer_test.go
+++ test/api_customer_test.go
@@ -11,10 +11,11 @@ package openapi
 
 import (
 	"context"
+	"testing"
+
+	openapiclient "github.com/Password-Management/API-Client"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"testing"
-	openapiclient "github.com/GIT_USER_ID/GIT_REPO_ID"
 )
 
 func Test_openapi_CustomerAPIService(t *testing.T) {
@@ -24,7 +25,7 @@ func Test_openapi_CustomerAPIService(t *testing.T) {
 
 	t.Run("Test CustomerAPIService CustomerPost", func(t *testing.T) {
 
-		t.Skip("skip test")  // remove to run test
+		t.Skip("skip test") // remove to run test
 
 		resp, httpRes, err := apiClient.CustomerAPI.CustomerPost(context.Background()).Execute()
 
@@ -36,7 +37,7 @@ func Test_openapi_CustomerAPIService(t *testing.T) {
 
 	t.Run("Test CustomerAPIService GetResultsGet", func(t *testing.T) {
 
-		t.Skip("skip test")  // remove to run test
+		t.Skip("skip test") // remove to run test
 
 		resp, httpRes, err := apiClient.CustomerAPI.GetResultsGet(context.Background()).Execute()
 
